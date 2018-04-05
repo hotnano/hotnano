b381fb377cdc34a1cd10ad97520c47a2ebbed80d
Mr. Nano <nanocoinio@gmail.com>
2018-04-05 20:16:29 +0200

    Use correct path for data.

--- bin/run.sh
+++ bin/run.sh
@@ -9,6 +9,7 @@ which docker &> /dev/null || { echo 'ERROR: docker not found in PATH'; exit 1; }
 cd "${SCRIPT_BASEDIR}/.."
 . ./.env
 
+set -x
 docker run \
 	--rm \
 	--tty \
@@ -16,7 +17,7 @@ docker run \
 	--publish ${LOCAL_PORT}:7075/udp \
 	--publish ${LOCAL_PORT}:7075/tcp \
 	--publish 127.0.0.1:${RPC_PORT}:7076 \
-	--volume "$HOME/RaiBlocks":/root \
+	--volume "$HOME/Library/RaiBlocks_HotNano":/root \
 	--name nano \
 	--hostname nano \
 	nanocurrency/nano
